405e1341cb3b3115205661f3892332415f2dd9f5
Alex Layne <alayne@redhat.com>
2026-07-20 15:51:50 -0400

    update hook script

--- pkg/hooks/posix_stdio.go
+++ pkg/hooks/posix_stdio.go
@@ -11,13 +11,35 @@ import (
 
 func posixStdioRun(cfg *config.Config, hook Hook, _ []string) (int, error) {
 	hookScript := `
-	exec 3>&1
-	exec 4>&2
+	# Define tmpdir and fifos using the current shell PID ($$)
+	_leaktk_hook_posix_stdio_tmpdir="${TMPDIR:-"/tmp"}/leaktk.$$"
+	_leaktk_hook_posix_stdio_stdout_fifo="${_leaktk_hook_posix_stdio_tmpdir}/stdout"
+	_leaktk_hook_posix_stdio_stderr_fifo="${_leaktk_hook_posix_stdio_tmpdir}/stderr"
 
-	exec > >(leaktk redact --kind Stdio >&3)
-	exec 2> >(leaktk redact --kind Stdio >&4)
+	# Create secure directory and named pipes
+	mkdir -p -m 0700 "${_leaktk_hook_posix_stdio_tmpdir}"
+	mkfifo           "${_leaktk_hook_posix_stdio_stdout_fifo}"
+	mkfifo           "${_leaktk_hook_posix_stdio_stderr_fifo}"
+
+	# Start background redaction daemons to listen to the pipes
+	leaktk redact --kind Stdio < "${_leaktk_hook_posix_stdio_stdout_fifo}" >&1 &
+	leaktk redact --kind Stdio < "${_leaktk_hook_posix_stdio_stderr_fifo}" >&2 &
+
+	# Redirect stdout and stderr
+	exec 1> "${_leaktk_hook_posix_stdio_stdout_fifo}"
+	exec 2> "${_leaktk_hook_posix_stdio_stderr_fifo}"
+
+	# Cleanup files from disk immediately. The open file descriptors in memory 
+	# keep the pipes active until the background processes terminate.
+	rm    "${_leaktk_hook_posix_stdio_stdout_fifo}"
+	rm    "${_leaktk_hook_posix_stdio_stderr_fifo}"
+	rmdir "${_leaktk_hook_posix_stdio_tmpdir}"
+
+	# Unset environment cleanup variables
+	unset _leaktk_hook_posix_stdio_tmpdir
+	unset _leaktk_hook_posix_stdio_stdout_fifo
+	unset _leaktk_hook_posix_stdio_stderr_fifo
 	`
-
 	_, err := fmt.Fprint(os.Stdout, strings.TrimSpace(hookScript))
 	if err != nil {
 		logger.Error("failed to print hook script: %v", err)
