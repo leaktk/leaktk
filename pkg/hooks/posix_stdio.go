a5fb374edc6dd40b13d7cd0ed269c65cede3f215
Alex Layne <alayne@redhat.com>
2026-07-21 16:49:02 -0400

    Update hook script to hid logs from scans

--- pkg/hooks/posix_stdio.go
+++ pkg/hooks/posix_stdio.go
@@ -11,6 +11,9 @@ import (
 
 func posixStdioRun(cfg *config.Config, hook Hook, _ []string) (int, error) {
 	hookScript := `
+	# Set user configurable var defaults
+	: "${LEAKTK_LOGGER_FILE:="/dev/null"}"
+
 	# Define tmpdir and fifos using the current shell PID ($$)
 	_leaktk_hook_posix_stdio_tmpdir="${TMPDIR:-"/tmp"}/leaktk.$$"
 	_leaktk_hook_posix_stdio_stdout_fifo="${_leaktk_hook_posix_stdio_tmpdir}/stdout"
@@ -22,8 +25,8 @@ func posixStdioRun(cfg *config.Config, hook Hook, _ []string) (int, error) {
 	mkfifo           "${_leaktk_hook_posix_stdio_stderr_fifo}"
 
 	# Start background redaction daemons to listen to the pipes
-	leaktk redact --kind Stdio < "${_leaktk_hook_posix_stdio_stdout_fifo}" >&1 &
-	leaktk redact --kind Stdio < "${_leaktk_hook_posix_stdio_stderr_fifo}" >&2 &
+	leaktk redact --kind Stdio < "${_leaktk_hook_posix_stdio_stdout_fifo}" 1>&1 2>"${LEAKTK_LOGGER_FILE}" &
+	leaktk redact --kind Stdio < "${_leaktk_hook_posix_stdio_stderr_fifo}" 1>&2 2>"${LEAKTK_LOGGER_FILE}" &
 
 	# Redirect stdout and stderr
 	exec 1> "${_leaktk_hook_posix_stdio_stdout_fifo}"
